ffee38c404c2a961a23030e8833712185c948f92
anshu0714 <anshujha14072002@gmail.com>
2025-05-23 21:21:34 +0530

    Added the settlement screen and functions.

--- app/(main)/person/[id]/page.jsx
+++ app/(main)/person/[id]/page.jsx
@@ -66,7 +66,7 @@ const PersonPage = () => {
 
           <div className="flex gap-2">
             <Button asChild variant={"outline"}>
-              <Link href={`settlements/user/${params.id}`}>
+              <Link href={`/settlements/user/${params.id}`}>
                 <ArrowLeftRight className="mr-2 h-4 w-4" />
                 Settle Up
               </Link>
